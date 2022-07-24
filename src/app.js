e2d6204df617f4f11ada25471c1f61422c708623
Ayeni Timilehin Ayodele <atayodele555@gmail.com>
2022-07-24 05:33:33 +0100

    uncomment

--- src/app.js
+++ src/app.js
@@ -50,31 +50,31 @@ app.get('/help/*', (req, res) => {
     })
 })
 
-// app.get('/weather', (req, res) => {
-//     if (!req.query.address) {
-//         return res.send({
-//             error: 'You must provide an address!'
-//         })
-//     }
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
 
-//     geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-//         if (error) {
-//             return res.send({ error })
-//         }
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
 
-//         forecast(latitude, longitude, (error, forecastData) => {
-//             if (error) {
-//                 return res.send({ error })
-//             }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
 
-//             res.send({
-//                 forecast: forecastData,
-//                 location,
-//                 address: req.query.address
-//             })
-//         })
-//     })
-// })
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
 
 app.get('/products', (req, res) => {
     if (!req.query.search) {
