abf441da0af539cf8963936e3d50a8d6150b212f
Ayeni Timilehin Ayodele <atayodele555@gmail.com>
2022-08-31 13:04:21 +0100

    ass:

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,4 +1,4 @@
 require('../src/db/mongoose')
 const User = require('../src/models.user')
 
-User.findByIdAndUpdate
+User.findByIdAndUpdate('')
--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { // both connectionString and db name
     userNewUrlParser: true,
-    useCreateIndex: true
-})
+    useCreateIndex: true,
+    useFindAndModify:false
+}) 
 
