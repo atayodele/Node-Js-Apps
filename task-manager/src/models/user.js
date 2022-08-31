2e4d1e399c18b5583895ebc838b81a49f9f9fa18
Ayeni Timilehin Ayodele <atayodele555@gmail.com>
2022-08-31 13:03:31 +0100

    push this

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,22 +1,43 @@
-const mongoose = require("../db/mongoose")
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 const User = mongoose.model("User", {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot contain "password"');
+      }
+    },
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a postive number");
+      }
+    },
   },
 });
-const me = new User({
-  name: "Andrew",
-  age: 24,
-});
 
-me.save()
-  .then((result) => {
-    console.log(me);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+module.exports = User;
