9449c4e56a4422066805875cea2abd5cd637c240
Ayeni Timilehin Ayodele <atayodele555@gmail.com>
2022-09-01 03:31:23 +0100

    push

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { // both connectionString and db name
-    userNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify:false
-}) 
+// mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { // both connectionString and db name
+//     userNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify:false,
+//     useUnifiedTopology: true 
+// }) 
 
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
+    useUnifiedTopology: true 
+}).then(()=>{
+    console.log('database connected')
+}).catch(err=>{
+    console.log('database not connected',err)
+})
