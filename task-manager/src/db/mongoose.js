971131c8888686392082f62569d6d5bd2b84a427
Ayeni Timilehin Ayodele <atayodele555@gmail.com>
2022-09-02 08:52:22 +0100

    emaill added and some other features

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose')
 
-// mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { // both connectionString and db name
-//     userNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify:false,
-//     useUnifiedTopology: true 
-// }) 
-
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
+mongoose.connect(process.env.MONGODB_URL,{
     userNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify:false,
