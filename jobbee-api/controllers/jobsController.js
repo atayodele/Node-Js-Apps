09e942bbc9e5df196440b8865dd341d9cdbad4bc
Ayeni Timilehin Ayodele <atayodele555@gmail.com>
2022-09-15 03:00:21 +0100

    jobbee-api in-progress

--- jobbee-api/controllers/jobsController.js
+++ jobbee-api/controllers/jobsController.js
@@ -13,6 +13,23 @@ exports.getJobs = async (req, res, next) => {
     });
 };
 
+// Get a single job with id and slug   =>  /api/v1/job/:id/:slug
+exports.getJob = (async (req, res, next) => {
+
+    const job = await Job.find({ $and: [{ _id: req.params.id }, { slug: req.params.slug }] })
+    if (!job || job.length === 0) {
+        res.status(404).json({
+            success: false,
+            message: 'Job not found.'
+        });
+    }
+
+    res.status(200).json({
+        success: true,
+        data: job
+    });
+});
+
 // Create a new Job   =>  /api/v1/job/create
 exports.newJob = async (req, res, next) => {
 
@@ -24,6 +41,48 @@ exports.newJob = async (req, res, next) => {
         data: job
     });
 };
+
+// Update a Job  =>  /api/v1/job/:id
+exports.updateJob = (async (req, res, next) => {
+    let job = await Job.findById(req.params.id);
+
+    if (!job) {
+        res.status(404).json({
+            success: false,
+            message: 'Job not found.'
+        });
+    }
+
+    job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        success: true,
+        message: 'Job is updated.',
+        data: job
+    });
+})
+
+// Delete a Job   =>  /api/v1/job/:id
+exports.deleteJob = (async (req, res, next) => {
+    let job = await Job.findById(req.params.id);
+
+    if (!job) {        
+        res.status(404).json({
+            success: false,
+            message: 'Job not found.'
+        });
+    }
+    
+    job = await Job.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+        success: true,
+        message: 'Job is deleted.'
+    })
+})
 
 // Search jobs with radius  =>  /api/v1/jobs/:zipcode/:distance
 exports.getJobsInRadius = async (req, res, next) => {
@@ -45,5 +104,35 @@ exports.getJobsInRadius = async (req, res, next) => {
         results: jobs.length,
         data: jobs
     });
+}
 
-};
+// Get stats about a topic(job)  =>  /api/v1/stats/:topic
+exports.jobStats = (async (req, res, next) => {
+    const stats = await Job.aggregate([
+        {
+            $match: { $text: { $search: "\"" + req.params.topic + "\"" } }
+        },
+        {
+            $group: {
+                _id: { $toUpper: '$experience' },
+                totalJobs: { $sum: 1 },
+                avgPosition: { $avg: '$positions' },
+                avgSalary: { $avg: '$salary' },
+                minSalary: { $min: '$salary' },
+                maxSalary: { $max: '$salary' }
+            }
+        }
+    ]);
+
+    if (stats.length === 0) {
+        res.status(400).json({
+            success: false,
+            message: `No stats found for - ${req.params.topic}`
+        });
+    }
+
+    res.status(200).json({
+        success: true,
+        data: stats
+    });
+});
--- jobbee-api/routes/jobs.js
+++ jobbee-api/routes/jobs.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = new express.Router();
-const { getJobs, newJob, getJobsInRadius } = require('../controllers/jobsController')
+const { getJobs, newJob, getJobsInRadius, updateJob, deleteJob, getJob, jobStats } = require('../controllers/jobsController')
 
 router.route('/jobs').get(getJobs)
+router.route('/jobs/:id/:slug').get(getJob)
 router.route('/jobs/create').post(newJob)
 router.route('/jobs/:zipcode/:distance').get(getJobsInRadius)
+router.route('/stats/:topic').get(jobStats)
+router.route('/jobs/:id').put(updateJob).delete(deleteJob)
 
 module.exports = router;
