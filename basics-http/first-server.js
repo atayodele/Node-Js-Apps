7ebe4b85ecd782df7e46f8987f56ff365fa3284e
Ayeni Timilehin Ayodele <atayodele555@gmail.com>
2022-09-30 06:59:41 +0100

    rename folder

--- basics-http/first-server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-    res.end('First web server')
-})
-
-server.listen(3000, () => {
-    console.log("Server has started")
-})
